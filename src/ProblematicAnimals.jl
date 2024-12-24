b400d686e6d3e78c9268c5ec91f67ea34e3954e6
g-gundam <g-gundam@users.noreply.github.com>
2024-12-24 14:35:41 -0800

    Add and export a why(a::AbstractAnimal) function

--- src/ProblematicAnimals.jl
+++ src/ProblematicAnimals.jl
@@ -7,9 +7,15 @@ struct Dog <: AbstractAnimal end
 sound(cat::Cat) = "meow"
 sound(dog::Dog) = "bark"
 
+# Structs defined in a notebook seem to become invisible while in this function.
+function why(a::AbstractAnimal)
+    sound(a)
+end
+
 export AbstractAnimal
 export Cat
 export Dog
 export sound
+export why
 
 end
